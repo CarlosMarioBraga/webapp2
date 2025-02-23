32b7a278aa6f6b210f4f2eaa4356280c47dabbb3
CarlosMarioBraga <44720881+CarlosMarioBraga@users.noreply.github.com>
2025-02-23 21:43:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ def index():
             nearvector = {
                 "vector": embedding
             }
-            result = bbddclient.query.get("Chunk", ["content", "pageNumber", "embeddingModel", "embeddingDate", "title", "author", "publicationDate", "identifier",  "documentType", "language", "rights"]).with_near_vector(nearvector).do()
+            result = bbddclient.query.get("Chunk", ["content", "pageNumber", "embeddingModel", "embeddingDate", "title", "author", "publicationDate", "identifier",  "documentType", "language", "rights"]).with_near_vector(nearvector).with_limit(10).do()
             # result = bbddclient.query.get("Chunk", ["content", "pageNumber", "embeddingModel", "embeddingDate"]).with_near_vector(nearvector).do()
             # result = bbddclient.query.get("Chunk",  ["content", "pageNumber", "embeddingModel", "embeddingDate", "title", "author", "publicationDate", "identifier",  "documentType", "language", "rights"]).with_limit(1).do()
             logger.info("Recibimos repuesta de weaviate e iniciamos la generación del prompt")          
